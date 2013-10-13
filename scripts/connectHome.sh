aa59d7e0bf0ed1c2c884a8512a986ac062563327
Ramon <user1@server1.fake1>
2013-10-13 07:34:20 -0400

    not working

---
+++ scripts/connectHome.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -n INFRA >/tmp/connectionManagerNamedPipe
+cat </tmp/connectionManagerNamedPipe
+echo -n 'homeWifi' >/tmp/connectionManagerNamedPipe
+cat </tmp/connectionManagerNamedPipe
+echo -n 'asdfASDF123!@#,,'>/tmp/connectionManagerNamedPipe
+cat </tmp/connectionManagerNamedPipe
