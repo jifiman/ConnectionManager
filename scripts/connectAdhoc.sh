4adcd540f763abd3a88ebd701bfaf5810309d825
Ramon <user1@server1.fake1>
2013-10-15 07:16:27 -0400

    github again

---
+++ scripts/connectAdhoc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo -n ADHOC >/tmp/connectionManagerNamedPipe
+cat </tmp/connectionManagerNamedPipe
+echo -n 'homeWifi' >/tmp/connectionManagerNamedPipe
+cat </tmp/connectionManagerNamedPipe
+echo -n 'asdfASDF123!@#,,'>/tmp/connectionManagerNamedPipe
+cat </tmp/connectionManagerNamedPipe
